5f8fdccfefcf894f19dc798e95df88a017ef6d68
constantcoder <garrett@constantcoder.io>
2021-05-04 14:58:27 -0500

    📖 DOC: Keywords and readme

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
 
-// clears the console before message
-clearConsole();
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
 
+welcome({
+  title: `Style Spin`,
+  tagLine: `Spin it to win it`,
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: `#bada55`,
+  color: `#000000`,
+  bold: true,
+  clear: true,
+});
 console.log(`
-Style Spin
-
-A command line interface to help you spin up your new project with the framework, library and styling you want.
+A command line interface to help you spin up your new project with the JavaScript framework, library and styling you want.
 
 Made by Garrett Lovell
 
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Style Spin
+
+> A command line interface to help you spin up your new project with the JavaScript framework, library and styling you want.
+
+## To use this CLI you can run the following commands:
+
+```sh
+npx spin
+```
